ff0cf0ecee2c18516de15cba9d80a0341d15f0c0
Alex Loren <lorenalexm@gmail.com>
2025-08-19 16:51:32 -0700

    Removes authToken from store, see 6d7669b

--- src/stores/data.js
+++ src/stores/data.js
@@ -1,9 +1,7 @@
 import { defineStore } from "pinia"
-import { useStorage } from "@vueuse/core"
 
 export let useDataStore = defineStore("app", {
 	state: () => ({
-		authToken: useStorage("authToken", ""),
 		servers: [],
 		currentServer: null,
 		libraries: [],
